f15c94d567c4889512dc064ea8bf6f83f2198de5
@SAYO <91386506+sayo-api@users.noreply.github.com>
2022-10-15 15:32:22 -0300

    Update bio.js

--- bio.js
+++ bio.js
@@ -1,6 +1,7 @@
 // Declarando as bibliotecas que vamos utilizar
 const Insta = require('./insta.js');
 const moment = require('moment');
+const time = moment().format('DD/MM HH:mm:ss')
 const client = new Insta.Client();
 
 
@@ -9,29 +10,21 @@ client.on('connected', () => {
 });
 
 client.on('messageCreate', async ayu => {
-
-async function bio () {
 	
-	
-		// Isso é opcional, foi feito para subtrair as horas de acordo com o horario aqui em São Paulo, já que esse sistema está rodando numa máquina no exterior (o meu sistema no caso, o seu provavelmente está em um fuso-horário diferente), caso queira remover deixe apenas a função format
-		data_display = moment().subtract(3, 'hours').format('lll');
 
-		// Mudando sua bio
-		// Caso ocorra algum erro, tente modificar a bio para que não ultrapasse 150 caracteres e não modifique o tempo no loop, deixe em 30min mesmo
-bior = `Your Body Language...
+let bio = 
+`Your Body Language...
 @biaahxzs
 Pᴇssᴏᴀs Tʀ¡sᴛᴇs Mᴏππᴇᴍ † † † † †. ..
-${data_display}`
-ayu.client.ig.account.setBiography(bior)
+${time.replace(' ', `\n`).replace(':', ' ')} ⏰`
+setInterval(async () => {	
+ayu.client.ig.account.setBiography(bio)
+	
 console.log(`Bio atualizada com sucesso (${data_display})`);
 	
-}
+}, 1 * 60000);
 
-// Adicionando um loop para mudar a bio de 30 em 30 min
-setInterval(async () => bio(), 1 * 60000);
 
-//  Mudando a bio assim que iniciar
-bio();
 })
 
 client.login('_breno.js', '34615194');
